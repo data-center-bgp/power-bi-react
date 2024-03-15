289400090c3dbbb6e25628695b8ee18d5c13347e
cga-bgp <cga.barokahperkasagroup@gmail.com>
2024-03-15 12:08:13 +0800

    style: add chakra ui for styling

--- src/components/Report.tsx
+++ src/components/Report.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 
 interface ReportProps {
-    src: string;
-    title: string;
+  src: string;
+  title: string;
 }
 
 const Report: React.FC<ReportProps> = ({ src, title }) => {
-    const iframe = `<iframe title=${title} src=${src} width="100%" height="100%" allowFullScreen="true"></iframe>`;
+  const iframe = `<iframe title=${title} src=${src} width="1280" height="720" allowFullScreen="true"></iframe>`;
 
-    return (
-        <div className="iframe-container" dangerouslySetInnerHTML={{ __html: iframe }}>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center">
+      <div
+        className="iframe-container w-full h-full border-0"
+        dangerouslySetInnerHTML={{ __html: iframe }}
+      ></div>
+    </div>
+  );
 };
 
 export default Report;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,25 @@
 import { Link } from "react-router-dom";
+import { Card, CardBody, CardHeader, CardFooter, Heading, Text, Button } from "@chakra-ui/react";
 
 function Homepage() {
   return (
-    <div>
-      <h1>Homepage</h1>
-      <Link to="/invoice-masbro">Dashboard Invoice Masbro</Link>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="mb-6 text-3xl font-bold text-gray-700">Homepage</h1>
+      <div>
+        <Card align="center">
+          <CardHeader>
+            <Heading size="md">Dashboard Invoice PT Mashud Bersaudara International</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              View the dashboard of invoice of PT Mashud Bersaudara International.
+            </Text>
+          </CardBody>
+          <CardFooter>
+            <Button as={Link} to="/invoice-masbro" colorScheme="blue">View dashboard</Button>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 }
