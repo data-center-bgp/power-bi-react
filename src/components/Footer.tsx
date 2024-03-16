afb397b5070e9152c89534bb200f24e61fbe69ef
cga-bgp <cga.barokahperkasagroup@gmail.com>
2024-03-16 09:25:07 +0800

    style: add footer and styling

---
+++ src/components/Footer.tsx
@@ -0,0 +1,88 @@
+import {
+  Stack,
+  HStack,
+  Link,
+  Image,
+  IconButton,
+  LinkProps,
+} from "@chakra-ui/react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { CgWebsite } from "react-icons/cg";
+
+const links = ["Blog", "Documentation", "Terms of Use", "Privacy Policy"];
+const accounts = [
+  {
+    url: "https://github.com/cgabarokahperkasagroup",
+    label: "GitHub Account",
+    type: "yellow",
+    icon: <FaGithub />,
+  },
+  {
+    url: "https://www.linkedin.com/company/barokah-perkasa-group/",
+    label: "LinkedIn Account",
+    type: "yellow",
+    icon: <FaLinkedin />,
+  },
+  {
+    url: "https://barokahperkasagroup.com/",
+    label: "Official Website",
+    type: "yellow",
+    icon: <CgWebsite />,
+  },
+];
+
+const CustomLink = ({ children, ...props }: LinkProps) => {
+  return (
+    <Link
+      href="#"
+      fontSize="sm"
+      _hover={{ textDecoration: "underline" }}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+};
+
+const Footer = () => {
+  return (
+    <Stack
+      maxW="5xl"
+      marginInline="auto"
+      p={8}
+      spacing={{ base: 8, md: 0 }}
+      justifyContent="space-between"
+      alignItems="center"
+      direction={{ base: "column", md: "row" }}
+    >
+      <Link href="/" isExternal>
+        <Image
+          w="100px"
+          src="/src/assets/logo-bpg.png"
+          alt="Barokah Perkasa Group"
+        />
+      </Link>
+      <HStack spacing={4} alignItems="center">
+        {links.map((link, index) => (
+          <CustomLink key={index}>{link}</CustomLink>
+        ))}
+      </HStack>
+      <Stack direction="row" spacing={5} pt={{ base: 4, md: 0 }} alignItems="center">
+        {accounts.map((account, index) => (
+          <IconButton
+            as={Link}
+            key={index}
+            href={account.url}
+            aria-label={account.label}
+            icon={account.icon}
+            colorScheme={account.type}
+            isExternal
+            rounded="md"
+          />
+        ))}
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Footer;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,26 +1,77 @@
 import { Link } from "react-router-dom";
-import { Card, CardBody, CardHeader, CardFooter, Heading, Text, Button } from "@chakra-ui/react";
+import Footer from "../components/Footer";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardFooter,
+  Heading,
+  Text,
+  Button,
+} from "@chakra-ui/react";
 
 function Homepage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="mb-6 text-3xl font-bold text-gray-700">Homepage</h1>
-      <div>
-        <Card align="center">
-          <CardHeader>
-            <Heading size="md">Dashboard Invoice PT Mashud Bersaudara International</Heading>
-          </CardHeader>
-          <CardBody>
-            <Text>
-              View the dashboard of invoice of PT Mashud Bersaudara International.
-            </Text>
-          </CardBody>
-          <CardFooter>
-            <Button as={Link} to="/invoice-masbro" colorScheme="blue">View dashboard</Button>
-          </CardFooter>
-        </Card>
+    <>
+      <div className="flex flex-col items-center justify-center flex-grow">
+        <h1 className="mb-6 text-3xl font-bold text-gray-700">
+          Archive for storing Power BI Dashboards
+        </h1>
+        <div>
+          <Card align="center">
+            <CardHeader>
+              <Heading size="md">
+                Dashboard Invoice PT Mashud Bersaudara International
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>
+                View the dashboard of invoice of PT Mashud Bersaudara
+                International.
+              </Text>
+            </CardBody>
+            <CardFooter>
+              <Button as={Link} to="/invoice-masbro" colorScheme="blue">
+                View dashboard
+              </Button>
+            </CardFooter>
+          </Card>
+          <Card align="center">
+            <CardHeader>
+              <Heading size="md">
+Coming soon...              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>
+Coming soon...
+              </Text>
+            </CardBody>
+            <CardFooter>
+              <Button as={Link} to="#" colorScheme="blue">
+                View dashboard
+              </Button>
+            </CardFooter>
+          </Card>
+          <Card align="center">
+            <CardHeader>
+              <Heading size="md">
+Coming soon...              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>
+Coming soon...
+              </Text>
+            </CardBody>
+            <CardFooter>
+              <Button as={Link} to="#" colorScheme="blue">
+                View dashboard
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
